dcba5a74eff939095b67e71900f402ca44b15cb5
James ZOU <youzhibicheng@users.noreply.github.com>
2016-04-20 18:28:03 +0800

    change the encoding into UTF-8
    change the encoding into UTF-8

--- rabbitmq/rabbitmq_web_stomp.sh
+++ rabbitmq/rabbitmq_web_stomp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # The Web-Stomp plugin is a simple bridge exposing the STOMP protocol over direct or emulated HTML5 WebSockets.
-# Stomp –≠“È
+# Stomp 
 sudo rabbitmq-plugins enable rabbitmq_web_stomp
 http://127.0.0.1:15674/stomp
 http://www.rabbitmq.com/web-stomp.html
